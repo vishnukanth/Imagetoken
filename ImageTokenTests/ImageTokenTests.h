09593b843256881e03f25ca3e486601aa20d081c
vishnu <vishnukanth@sapnasolutions.com>
2011-12-23 12:49:45 +0530

    made changes

---
+++ ImageTokenTests/ImageTokenTests.h
@@ -0,0 +1,13 @@
+//
+//  ImageTokenTests.h
+//  ImageTokenTests
+//
+//  Created by Admin on 23/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ImageTokenTests : SenTestCase
+
+@end
---
+++ ImageTokenTests/ImageTokenTests.m
@@ -0,0 +1,32 @@
+//
+//  ImageTokenTests.m
+//  ImageTokenTests
+//
+//  Created by Admin on 23/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ImageTokenTests.h"
+
+@implementation ImageTokenTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ImageTokenTests");
+}
+
+@end
